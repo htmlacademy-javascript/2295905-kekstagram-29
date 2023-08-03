ba17eb14860bfbde376db53f85d846e4313d7aa8
negara999 <negara.sergiu2@gmail.com>
2023-08-04 02:06:45 +0300

    Fixed .catch

--- js/api.js
+++ js/api.js
@@ -21,10 +21,10 @@ const load = (route, errorText, method = Method.GET, body = null) =>
         throw new Error();
       }
       return response.json();
-    });
+    })
     .catch(() => {
       throw new Error(errorText);
-      });
+    });
 
 const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
 
