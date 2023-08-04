90b64f13e72d1943e7824f68bafd7388c5cd6ad3
negara999 <negara.sergiu2@gmail.com>
2023-08-05 00:08:53 +0300

    Перламутровые пуговицы и Фото на память

---
+++ js/upload-image.js
@@ -0,0 +1,22 @@
+const FILE_TYPES = ['png', 'jpg', 'jpeg'];
+
+const fileChooser = document.querySelector('.img-upload__input');
+const imagePreview = document.querySelector('.img-upload__preview img');
+const form = document.querySelector('.img-upload__form');
+const previewEffects = form.querySelectorAll('.effects__preview');
+
+const showPreviewImage = () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if(matches) {
+    imagePreview.src = URL.createObjectURL(file);
+    previewEffects.forEach((previewEffect) => {
+      previewEffect.style.backgroundImage = `url(${imagePreview.src})`;
+    });
+  }
+};
+
+export { showPreviewImage };
